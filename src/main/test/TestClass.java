e3e3f141770c73a30b587561385f784a38eb2d8a
Programersheep <32678632+Programersheep@users.noreply.github.com>
2019-04-17 21:31:23 +0800

    Add files via upload

--- src/main/test/TestClass.java
+++ src/main/test/TestClass.java
@@ -11,68 +11,18 @@ import org.testng.annotations.Test;
 import java.io.*;
 import java.util.ArrayList;
     class TestClass {
-        public final String URL=System.getProperty("user.dir")+"/src/main/resources/";
+        public  static final String URL=System.getProperty("user.dir")+"/src/main/resources/";
         public  static String FILE;
         @Test
         @Parameters({"FILENAME"})
         public void getFile(String fileName){
             FILE=fileName;
             TestNG testNG = new TestNG();
-            Class []classes={TestClass.class};
+            Class []classes={Test2.class};
             testNG.setTestClasses(classes);
             testNG.run();
         }
-        @Test(dataProvider = "user")
-        public void test(String a,String b,String c){
-            int ic=Integer.parseInt(c);
-            Search s=new Search();
-            int res=s.TestIN(a,b,ic);
-            Assert.assertEquals(res,ic);
-        }
-        @DataProvider(name = "user")
-        public  Object[][] getExcelData(){
-            ArrayList<ArrayList<String>>flight=new ArrayList<ArrayList<String>>();
-            try{
-                // 创建输入流，读取Excel
-                File file=new File(URL+FILE);
-                InputStream is = new FileInputStream(file);
-                // jxl提供的Workbook类
-                Workbook wb = Workbook.getWorkbook(is);
-                // Excel的页签数量
-                int sheet_size = wb.getNumberOfSheets();
-                for(int i=0;i<sheet_size;i++){
-                    Sheet readsheet=wb.getSheet(i);
-                    int rsRows=readsheet.getRows();
-                    int rsClomns=readsheet.getColumns();
-                    for(int j=0;j<rsRows;j++){
-                        ArrayList<String>info=new ArrayList<String>();
-                        for(int k=0;k<rsClomns;k++){
-                            Cell cell=readsheet.getCell(k,j);
-                            String str=cell.getContents();
-                            info.add(str);
-                        }
-                        flight.add(info);
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (BiffException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // 定义一个object[][] 的二维数组，存放list中的值
-            Object[][] results = new Object[flight.size()][];
-            // 设置二维数组每行的值，
-            for (int a = 0; a < flight.size(); a++) {
-                results[a]=new Object[3];
-                for(int j=0;j<3;j++){
-                    results[a][j]=flight.get(a).get(j);
-                    //System.out.println(results[a][j]);
-                }
-            }
-            return results;
-        }
+
         /*
     @DataProvider(name="user")
     public Object[][] Users(){
@@ -155,6 +105,61 @@ import java.util.ArrayList;
         };
     }
     */
+
     }
+class Test2{
+    @Test(dataProvider = "user")
+    public void test(String a,String b,String c){
+        int ic=Integer.parseInt(c);
+        Search s=new Search();
+        int res=s.TestIN(a,b,ic);
+        Assert.assertEquals(res,ic);
+    }
+    @DataProvider(name = "user")
+    public  Object[][] getExcelData(){
+        ArrayList<ArrayList<String>>flight=new ArrayList<ArrayList<String>>();
+        try{
+            // 创建输入流，读取Excel
+            File file=new File(TestClass.URL+TestClass.FILE);
+            InputStream is = new FileInputStream(file);
+            // jxl提供的Workbook类
+            Workbook wb = Workbook.getWorkbook(is);
+            // Excel的页签数量
+            int sheet_size = wb.getNumberOfSheets();
+            for(int i=0;i<sheet_size;i++){
+                Sheet readsheet=wb.getSheet(i);
+                int rsRows=readsheet.getRows();
+                int rsClomns=readsheet.getColumns();
+                for(int j=0;j<rsRows;j++){
+                    ArrayList<String>info=new ArrayList<String>();
+                    for(int k=0;k<rsClomns;k++){
+                        Cell cell=readsheet.getCell(k,j);
+                        String str=cell.getContents();
+                        info.add(str);
+                    }
+                    flight.add(info);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 定义一个object[][] 的二维数组，存放list中的值
+        Object[][] results = new Object[flight.size()][];
+        // 设置二维数组每行的值，
+        for (int a = 0; a < flight.size(); a++) {
+            results[a]=new Object[3];
+            for(int j=0;j<3;j++){
+                results[a][j]=flight.get(a).get(j);
+                //System.out.println(results[a][j]);
+            }
+        }
+        return results;
+    }
+}
+
 
 
